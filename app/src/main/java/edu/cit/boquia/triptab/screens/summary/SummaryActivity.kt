2278ae72c0e58c97f7650507dfdfaa4a19448b72
l24x11w28 <altairboquia16@gmail.com>
2026-05-13 11:44:22 +0800

    "implemented Pie Chart for Summary screen"

--- app/src/main/java/edu/cit/boquia/triptab/screens/summary/SummaryActivity.kt
+++ app/src/main/java/edu/cit/boquia/triptab/screens/summary/SummaryActivity.kt
@@ -4,22 +4,102 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.Typeface
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.MPPointF
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.cit.boquia.triptab.R
 import edu.cit.boquia.triptab.screens.main.MainActivity
 import edu.cit.boquia.triptab.screens.profile.ProfileActivity
 
 class SummaryActivity : AppCompatActivity(){
+    // pie chart var
+    lateinit var pieChart: PieChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_summary)
 
-        // Retrieve data from intent
-        val name = intent.getStringExtra("name")
-        val email = intent.getStringExtra("email")
-        val birthDate = intent.getStringExtra("birthDate")
-        val phoneNo = intent.getStringExtra("phoneNo")
+        // pie chart section
+        pieChart = findViewById(R.id.summaryPieChart)
+        pieChart.setUsePercentValues(true) // setting user %
+        pieChart.getDescription().setEnabled(false) // remove desc
+        pieChart.setExtraOffsets(5f, 10f, 5f, 5f) // offset
+
+        // drag
+        pieChart.setDragDecelerationFrictionCoef(0.95f)
+
+        // center hole
+        pieChart.setDrawHoleEnabled(true)
+        pieChart.setHoleColor(Color.WHITE)
+
+        // circle color & alpha
+        pieChart.setTransparentCircleColor(Color.WHITE)
+        pieChart.setTransparentCircleAlpha(110)
+
+        // hole radius
+        pieChart.setHoleRadius(60f)
+        pieChart.setTransparentCircleRadius(62f)
+
+        // center text
+        pieChart.setDrawCenterText(true)
+
+        // rotation
+        pieChart.setRotationEnabled(true)
+        pieChart.setHighlightPerTapEnabled(true)
+
+        // disable legend
+        pieChart.legend.isEnabled = false
+        pieChart.setEntryLabelColor(Color.WHITE)
+        pieChart.setEntryLabelTextSize(12f)
+
+        // sample data set
+
+        val entries: ArrayList<PieEntry> = ArrayList()
+        entries.add(PieEntry(70f))
+        entries.add(PieEntry(20f))
+        entries.add(PieEntry(10f))
+
+        // on below line we are setting pie data set
+        val dataSet = PieDataSet(entries, "Mobile OS")
+
+        // on below line we are setting icons.
+        dataSet.setDrawIcons(false)
+
+        // on below line we are setting slice for pie
+        dataSet.sliceSpace = 3f
+        dataSet.iconsOffset = MPPointF(0f, 40f)
+        dataSet.selectionShift = 5f
+
+        // add a lot of colors to list
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(resources.getColor(R.color.purple_200))
+        colors.add(resources.getColor(R.color.yellow))
+        colors.add(resources.getColor(R.color.red))
+
+        // on below line we are setting colors.
+        dataSet.colors = colors
+
+        // on below line we are setting pie data set
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter())
+        data.setValueTextSize(15f)
+        data.setValueTypeface(Typeface.DEFAULT_BOLD)
+        data.setValueTextColor(Color.WHITE)
+        pieChart.setData(data)
+
+        // undo all highlights
+        pieChart.highlightValues(null)
+        // load chart
+        pieChart.invalidate()
+
+
 
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_summary)
